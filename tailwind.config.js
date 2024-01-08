8de5dcafb8368922b1ad6b3095586427298db8dc
Gomez jacob <jacobgomez325@gmail.com>
2024-01-08 23:18:14 +0100

    chore: initiate tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary: "#FBCF16",
+        secondary: "#2F4858"
+      },
+    },
+  },
+  plugins: [],
+}
+
